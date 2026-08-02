2c08a7aeadcc05f80e2d273ad6375fa7c8c6c157
sadra <sadrataghavi1383@gmail.com>
2026-08-02 20:01:40 +0330

    feat(nav and footer import)

--- app/index.tsx
+++ app/index.tsx
@@ -1,20 +1,37 @@
-import { Route, Router } from '@solidjs/router'
+import { Route, Router, RouteSectionProps } from '@solidjs/router'
 import { render } from 'solid-js/web'
 
 // import Alert from 'comps/alert'
 
-import Home from 'pages/home'
-import './style/base.scss'
+import Footer from 'layout/footer'
+import Navbar from 'layout/navbar'
+import { lazy } from 'solid-js'
 import './style/base.scss'
 import './style/theme.scss'
+
+const Home = lazy(() => import('pages/home'))
+const UnderConstruction = lazy(() => import('pages/underConstruction'))
+
+const Rootlayout = (P: RouteSectionProps) => {
+    return (
+        <>
+            <Navbar />
+
+            {P.children}
+
+            <Footer />
+        </>
+    )
+}
 
 const Root = () => {
     return (
         <>
-            <Router>
-                <Route path='/' component={Home}></Route>
+            <Router base='/' root={Rootlayout}>
+                <Route path={'/'} component={Home}></Route>
+
+                <Route path='*' component={UnderConstruction} />
             </Router>
-            {/* <Alert /> */}
         </>
     )
 }
