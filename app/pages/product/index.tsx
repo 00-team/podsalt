901e61c4f0bdbc3552dd54dcfee77ccc267cb01a
sadra <sadrataghavi1383@gmail.com>
2026-08-06 12:04:18 +0330

    feat(products): img

--- app/pages/product/index.tsx
+++ app/pages/product/index.tsx
@@ -1,8 +1,9 @@
-import { useNavigate, useParams } from '@solidjs/router'
-import { Component, onMount } from 'solid-js'
+import { A, useNavigate, useParams } from '@solidjs/router'
+import { Component, onMount, Show } from 'solid-js'
 
-import { hasProductById } from 'shared/products'
-import { unwrap } from 'solid-js/store'
+import { ArrowRight2Icon } from 'icons/main'
+import { hasProductById, PRODUCT } from 'shared/products'
+import { createStore } from 'solid-js/store'
 import './style/product.scss'
 
 const Product: Component = () => {
@@ -10,15 +11,56 @@ const Product: Component = () => {
 
     const nav = useNavigate()
 
+    type STATE = {
+        product: PRODUCT | null
+    }
+    const [state, setState] = createStore<STATE>({
+        product: null,
+    })
+
     onMount(() => {
         const id = param.id
         if (!id || !hasProductById(id))
             return nav('/not-found', { replace: true })
 
-        console.log(unwrap(param))
+        setState('product', hasProductById(id))
     })
 
-    return <main class='product-page-container'></main>
+    return (
+        <main class='product-page-container'>
+            <div class='top-nav title_smaller'>
+                <A class='link' href='/'>
+                    Home
+                </A>
+                <div class='icon'>
+                    <ArrowRight2Icon />
+                </div>
+                <A class='link' href='/products'>
+                    E-Liquids
+                </A>
+                <div class='icon'>
+                    <ArrowRight2Icon />
+                </div>
+                <strong class='link'>
+                    Pod Salt Bar X {state.product?.name}
+                </strong>
+            </div>
+
+            <div class='product-info-container'>
+                <div class='product-image'>
+                    <Show when={state.product?.img}>
+                        <img
+                            src={state.product?.img}
+                            fetchpriority='high'
+                            decoding='async'
+                        />
+                        <div class='is-new title_smaller'>New</div>
+                    </Show>
+                </div>
+                <div class='product-infos'></div>
+            </div>
+        </main>
+    )
 }
 
 export default Product
